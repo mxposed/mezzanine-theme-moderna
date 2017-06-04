520d530af1b33c8894c94c11ee1e9aac48f8965d
Nikolay Markov <markov@yandex-team.ru>
2017-06-04 04:15:33 +0300

    Fix setup to install static files

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 setup(
     name='mezzanine-theme-moderna',
     version='0.1',
@@ -15,5 +22,6 @@ setup(
     author_email='a4blank@yahoo.com',
     license='MIT',
     packages=['moderna'],
+    package_data=dict(moderna=package_files('moderna/static') + package_files('moderna/templates')),
     download_url='https://github.com/mxposed/mezzanine-theme-moderna/archive/v0.1.tar.gz',
 )
